b95be03f44d1061eac489f21ef2505e4848f088e
Luigi Morel <morelmiles@gmail.com>
2021-05-10 08:11:14 +0300

    Some work on the messenger section

--- src/ui/messenger/components/messengerChat.tsx
+++ src/ui/messenger/components/messengerChat.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Box, Grid, Typography } from '@material-ui/core';
-
+import { ReactComponent as LightningIcon } from '../../../assets/feedIcons/Lightining-icon.svg';
+import { ReactComponent as Ellipsis } from '../../../assets/feedIcons/Ellipsis.svg';
 import placeholderImage from '../../../assets/person.jpg';
+import { FaEllipsisH, FaSearch, FaThumbtack } from 'react-icons/fa';
+import { SearchRounded } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     mainContainer: {
         width: '700px',
+        
     },
     header: {
-        height: '110px',
+        height: 'auto   ',
+        boxShadow: '6px 6px 12px #b8b9be, -6px -6px 12px #ffffff !important',
     },
     messageContainer: {
         marginLeft: '3rem',
@@ -17,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '1rem',
         '&::hover': {
             boxShadow:
-                'ins    et 6px 6px 12px #b8b9be, -6px -6px 12px #ffffff !important',
+                'inset 6px 6px 12px #b8b9be, -6px -6px 12px #ffffff !important',
         },
     },
     groupAvatar: {
@@ -38,15 +43,28 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: 600,
         paddingBottom: '25px',
     },
+    iconsContainer: {
+        margin: '2rem',
+    },
+    pinnedMessage: {
+        height: '75px',
+    },
 }));
 const MessengerChat = () => {
     const classes = useStyles();
 
+    const [MessengerMenu, setMessengerMenu] = useState(false);
+
     return (
         <div className={classes.mainContainer}>
-            <div className={classes.header}>
+            <Box
+                className={classes.header}
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+            >
                 <Box
-                    display="flex"             
+                    display="flex"
                     flexDirection="row"
                     className={classes.messageContainer}
                 >
@@ -58,18 +76,51 @@ const MessengerChat = () => {
                         />
                     </Box>
                     <Box className={classes.textContainer}>
-                        <Typography component="p" className={classes.groupHeader}>
+                        <Typography
+                            component="p"
+                            className={classes.groupHeader}
+                        >
                             Into The Cryptoverse{' '}
                         </Typography>
                         <Typography component="p">
-                            27, 145 members, {' '}
+                            27, 145 members,{' '}
                             <span style={{ color: '#1D9F6E' }}>
                                 1649 online{' '}
-                                </span>
+                            </span>
                         </Typography>
                     </Box>
                 </Box>
-            </div>
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    className={classes.iconsContainer}
+                >
+                    <SearchRounded color="secondary" fontSize="large" />
+                    <Ellipsis color="#69BDA2" height="2rem" />
+                </Box>
+            </Box>
+
+            <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+                className={classes.pinnedMessage}
+            >
+                <Box>
+                    <Typography variant="body1" style={{ color: '#39AB80' }}>
+                        Pinned Message
+                    </Typography>
+
+                    <Typography component="p">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry
+                    </Typography>
+                </Box>
+
+                <Box>
+                    <FaThumbtack color="#69BDA2" fontSize="1.5rem" />
+                </Box>
+            </Box>
         </div>
     );
 };
